25ff74d7096223a1bfadac0ba5c19981a320cf11
PABLO ENTONY SILVA SANTOS <pablo.entony@nxgold.com.br>
2023-09-28 09:47:24 -0300

    Salvo 28/09/2023

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,10 @@ let seconds = 0;
 let milliseconds = 0;
 let isPaused = false;
 
-startBtn.addEventListener("click", startTimer)
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTimer);
+resumeBtn.addEventListener("click", resumeTimer);
+resetBtn.addEventListener("click", resetTimer);
 
 function startTimer() {
     interval = setInterval(() => {
@@ -33,6 +36,36 @@ function startTimer() {
             millisecondsEl.textContent = formatMilliseconds(milliseconds);
         }
     }, 10)
+
+    startBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+}
+
+function pauseTimer() {
+    isPaused = true;
+    pauseBtn.style.display = "none";
+    resumeBtn.style.display = "block";
+}
+
+function resumeTimer() {
+    isPaused = false;
+    pauseBtn.style.display = "block";
+    resumeBtn.style.display = "none";
+}
+
+function resetTimer() {
+    clearInterval(interval);
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+
+    minutesEl.textContent = "00";
+    secondsEl.textContent = "00";
+    millisecondsEl.textContent = "000";
+
+    startBtn.style.display = "block";
+    pauseBtn.style.display = "none";
+    resumeBtn.style.display = "none";
 }
 
 function formatTime(time) {
